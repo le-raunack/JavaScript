472f6896b9407924dfb3e601a9705915d6f786b2
le-raunack <le-raunack@users.noreply.github.com>
2018-11-04 10:56:16 +0530

    JavaScript Attempt 2.0.0.0

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 "use strict";
 let time = new Date();
 document.getElementById("date").innerHTML =
@@ -11,7 +10,7 @@ document.getElementById("date").innerHTML =
 
 function bin2dec() {
   let bin = document.getElementById("binary").value;
-  if(bin==""){
+  if (bin == "") {
     alert("*Binary number required!");
   }
   let dec = 0,
@@ -30,13 +29,13 @@ function bin2dec() {
 
 function oct2dec() {
   let oct = document.getElementById("octal").value;
-  if(oct==""){
+  if (oct == "") {
     alert("*Octal number required!");
   }
   let dec = 0,
     rem = 0,
     dic = 0,
-    count = 0; 
+    count = 0;
   while (oct > 0) {
     rem = oct % 10;
     dic = rem * Math.pow(8, count);
@@ -49,7 +48,7 @@ function oct2dec() {
 
 function hex2dec() {
   let hex = document.getElementById("hexadec").value;
-  if(hex==""){
+  if (hex == "") {
     alert("*Hexadecimal number required!");
   }
   let dec = 0;
@@ -90,4 +89,130 @@ function hex2dec() {
   }
 
   document.getElementById("decim").value = sum;
+}
+
+function Key_1() {
+  let key = document.getElementById("1").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_2() {
+  let key = document.getElementById("2").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_3() {
+  let key = document.getElementById("3").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_4() {
+  let key = document.getElementById("4").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_5() {
+  let key = document.getElementById("5").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_6() {
+  let key = document.getElementById("6").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_7() {
+  let key = document.getElementById("7").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_8() {
+  let key = document.getElementById("8").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_9() {
+  let key = document.getElementById("9").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_9() {
+  let key = document.getElementById("9").value;
+  document.getElementById("calcscreen").value += key;
+}
+
+function Key_plus() {
+  document.getElementById("calcscreen").value += "+";
+}
+
+function Key_minus() {
+  document.getElementById("calcscreen").value += "-";
+}
+
+function Key_mul() {
+  document.getElementById("calcscreen").value += "*";
+}
+
+function Key_div() {
+  document.getElementById("calcscreen").value += "/";
+}
+
+function Key_decimal() {
+  document.getElementById("calcscreen").value += ".";
+}
+
+function Calculator() {
+  let operandStack = [""],
+    operationStack = [],
+    OperationClass = ["+", "-", "*", "/", "="];
+  let input = document.getElementById("calcscreen").value;
+  if (input.indexOf("=") === -1) {
+    input += "=";
+  }
+  let i = input.length;
+  for (i = 0; i < input.length; i++) {
+    let newNo = input.charAt(i);
+    if (OperationClass.indexOf(newNo) === -1) {
+      let latestNo = operandStack.pop();
+      let totalNo = latestNo + newNo;
+      operandStack.push(totalNo);
+    } else {
+      if (operationStack.length === 0) {
+        operationStack.push(newNo);
+      } else {
+        let Operation = operationStack.pop(),
+          no1 = parseFloat(operandStack.pop()),
+          result = 0,
+          no2 = parseFloat(operandStack.pop());
+        switch (Operation) {
+          case "+":
+            result = no1 + no2;
+            operandStack.push(result);
+            break;
+          case "-":
+            result = no2 - no1;
+            operandStack.push(result);
+            break;
+          case "*":
+            result = no1 * no2;
+            operandStack.push(result);
+            break;
+          case "/":
+            result = no2 / no1;
+            operandStack.push(result);
+            break;
+        }
+        if (newNo === "=") {
+          if (operandStack[operandStack.length - 1] == NaN) {
+            alert("Improper insertion, please verify");
+          }
+          document.getElementById("calcscreen").value =
+            operandStack[operandStack.length - 1];
+        } else {
+          operationStack.push(newNo);
+        }
+      }
+      operandStack.push("");
+    }
+  }
 }
